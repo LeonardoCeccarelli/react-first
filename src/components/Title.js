761f830addfceb6a49a6b0b15676511641bfdad2
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-12-29 15:34:52 +0100

    add new file css and new paragraph

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Title = () => {
     return (
-        <h1 className='main_title'>First application with ReactJS</h1>
+        <>
+            <h1 className='main_title'>First application with ReactJS</h1>
+            <p>lorem ipsum dolor</p>
+        </>
     )
 }
 
--- src/style.js
+++ src/style.js
@@ -1,1 +1,2 @@
 import './styles/title.css'
+import './styles/paragraph.css'
